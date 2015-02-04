8063c98c23602613b012a5faa84eb2c4ba9f0dec
Lucas <lucasdnd@gmail.com>
2015-02-04 18:10:27 -0200

    Added rocket class

---
+++ rocket.js
@@ -0,0 +1,12 @@
+var Rocket = function() {
+  
+  this.x = 0.0;
+  this.y = 0.0;
+
+  this.speedX = 0.0;
+  this.speedY = 0.0;
+
+  this.launch = function() {
+    console.log("orbit or bust!");
+  }
+};
